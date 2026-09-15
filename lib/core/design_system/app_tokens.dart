5195624e954c6e8fa6d7c595e5cfac199e6ffad3
dajarony <59666460+dajarony@users.noreply.github.com>
2026-09-15 14:44:24 +0100

    refactor: add component size tokens

---
+++ lib/core/design_system/app_tokens.dart
@@ -0,0 +1,9 @@
+class AppTokens {
+  AppTokens._();
+
+  static const double movementPadExtent = 150;
+  static const double movementIconSize = 34;
+  static const double spellIconSize = 34;
+  static const double hudOverlayOpacity = 0.45;
+  static const double controlOverlayOpacity = 0.38;
+}
