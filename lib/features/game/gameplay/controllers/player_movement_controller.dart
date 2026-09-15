6dc74bce793526cd6be65cc7f0de3e8832eca8f0
dajarony <59666460+dajarony@users.noreply.github.com>
2026-09-15 14:43:34 +0100

    refactor: isolate player movement controller

---
+++ lib/features/game/gameplay/controllers/player_movement_controller.dart
@@ -0,0 +1,32 @@
+import 'package:flame/components.dart';
+
+import '../components/components.dart';
+
+class PlayerMovementController {
+  PlayerMovementController({
+    required MageComponent player,
+    required Vector2 Function() worldSize,
+  })  : _player = player,
+        _worldSize = worldSize;
+
+  static const double _moveStep = 24;
+  static const double _worldMargin = 28;
+
+  final MageComponent _player;
+  final Vector2 Function() _worldSize;
+
+  void moveUp() => move(Vector2(0, -_moveStep));
+  void moveDown() => move(Vector2(0, _moveStep));
+  void moveLeft() => move(Vector2(-_moveStep, 0));
+  void moveRight() => move(Vector2(_moveStep, 0));
+
+  void move(Vector2 delta) {
+    final next = _player.position + delta;
+    final size = _worldSize();
+
+    _player.position = Vector2(
+      next.x.clamp(_worldMargin, size.x - _worldMargin),
+      next.y.clamp(_worldMargin, size.y - _worldMargin),
+    );
+  }
+}
