d397444db6466139ef93e2d67bfb2db10580cc9e
dajarony <59666460+dajarony@users.noreply.github.com>
2026-09-15 14:43:56 +0100

    refactor: make MagycGame a composition root

---
+++ lib/features/game/gameplay/magyc_game.dart
@@ -0,0 +1,31 @@
+import 'package:flame/game.dart';
+
+import 'components/components.dart';
+import 'controllers/controllers.dart';
+
+class MagycGame extends FlameGame {
+  late final MageComponent player;
+  late final PlayerMovementController movement;
+  late final SpellCastingController spells;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    add(IsometricGroundComponent(size: size));
+
+    player = MageComponent(
+      position: Vector2(size.x * 0.5, size.y * 0.58),
+    );
+    add(player);
+
+    movement = PlayerMovementController(
+      player: player,
+      worldSize: () => size,
+    );
+    spells = SpellCastingController(
+      world: this,
+      player: player,
+    );
+  }
+}
